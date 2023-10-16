9b8aa3ed7a87da571c0c6241721244aed5763453
MeatMaster5 <143038235+MeatMaster5@users.noreply.github.com>
2023-10-16 17:54:10 -0500

    Create lab1.py

---
+++ lab1.py
@@ -0,0 +1,30 @@
+def convert_kg(value):
+	# convert kg to lbs and oz
+    oz = str(value * 35.274)
+    lb = str(value * 2.20462)
+    value = str(value)
+    print(value + " kilograms = " + oz +" ounces")
+    print(value + " kilograms = " + lb +" pounds")
+
+
+
+def convert_pounds(value):
+	# convert lbs to kg and oz
+    kg = str(value * .453592)
+    oz = str(value * 16)
+    value = str(value)
+    print(value + " pounds = "+kg+" kilograms")
+    print(value+ " pounds = "+oz+" ounces")
+    
+def convert_ounces(value):
+	# convert oz to kg and lbs
+    kg = str(value * .0283)
+    lb = str(value * .0625)
+    value = str(value)
+    print(value + " ounces = "+kg+" kilograms")
+    print(value + " ounces = "+lb+" pounds")
+
+
+convert_kg(10)
+convert_pounds(10)
+convert_ounces(10)
